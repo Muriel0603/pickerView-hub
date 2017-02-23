2534d3b07e1bff9144339f8d6a2b286ac3eb2317
Muriel <muriel.jin@both.com>
2017-02-23 18:30:48 +0800

    first commit

---
+++ ios/RCTBEEPickerManager/RCTBEEPickerManager.h
@@ -0,0 +1,16 @@
+//
+//  RCTBEEPickerManager.h
+//  RCTBEEPickerManager
+//
+//  Created by MFHJ-DZ-001-417 on 16/9/6.
+//  Copyright © 2016年 MFHJ-DZ-001-417. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+#import "BzwPicker.h"
+
+
+@interface RCTBEEPickerManager : NSObject<RCTBridgeModule,confirmDelegate>
+
+@end
